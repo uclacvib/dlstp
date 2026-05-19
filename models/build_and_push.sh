d59c0552b96824531312062c33de2028c17e3a8a
pangyuteng <pteng@mednet.ucla.edu>
2026-05-19 12:27:05 -0700

    added report containers

--- models/build_and_push.sh
+++ models/build_and_push.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 export DOCKERTAG="-v0.0.1"
 
-docker build -t pangyuteng/dlstp:3duxnet${DOCKERTAG} 3duxnet/docker
+docker build -t pangyuteng/dlstp:3duxnet${DOCKERTAG} -f 3duxnet/docker/Dockerfile 3duxnet/docker
 docker push pangyuteng/dlstp:3duxnet${DOCKERTAG}
 
-docker build -t pangyuteng/dlstp:nnunet${DOCKERTAG} nnUnet/docker
+docker build -t pangyuteng/dlstp:nnunet${DOCKERTAG} -f nnUnet/docker/Dockerfile nnUnet/docker
 docker push pangyuteng/dlstp:nnunet${DOCKERTAG}
 
-docker build -t pangyuteng/dlstp:umamba${DOCKERTAG} umamba/docker
+docker build -t pangyuteng/dlstp:umamba${DOCKERTAG} -f umamba/docker/Dockerfile umamba/docker
 docker push pangyuteng/dlstp:umamba${DOCKERTAG}
 
-docker build -t pangyuteng/dlstp:unetr${DOCKERTAG} unetr/docker
+docker build -t pangyuteng/dlstp:unetr${DOCKERTAG} -f unetr/docker/Dockerfile unetr/docker
 docker push pangyuteng/dlstp:unetr${DOCKERTAG}
+
+docker build -t pangyuteng/dlstp:3duxnet-report${DOCKERTAG} -f 3duxnet/docker/Dockerfile.report 3duxnet/docker
+docker push pangyuteng/dlstp:3duxnet-report${DOCKERTAG}
+
+docker build -t pangyuteng/dlstp:nnunet-report${DOCKERTAG} -f nnUnet/docker/Dockerfile.report nnUnet/docker
+docker push pangyuteng/dlstp:nnunet-report${DOCKERTAG}
+
+docker build -t pangyuteng/dlstp:umamba-report${DOCKERTAG} -f umamba/docker/Dockerfile.report umamba/docker
+docker push pangyuteng/dlstp:umamba-report${DOCKERTAG}
+
+docker build -t pangyuteng/dlstp:unetr-report${DOCKERTAG} -f unetr/docker/Dockerfile.report unetr/docker
+docker push pangyuteng/dlstp:unetr-report${DOCKERTAG}
+
